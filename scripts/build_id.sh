bc7c0c35f37cca7e2f373935988e501d019a1ef3
Joe Banks <joe@jb3.dev>
2025-10-23 04:13:26 +0100

    Make build ID multi-platform

--- scripts/build_id.sh
+++ scripts/build_id.sh
@@ -1,8 +1,17 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -eux
 
 DATE=$(date +"%Y.%m.%d")
-COMMITS=$(git rev-list --count HEAD --since $(date -v-1d +'%Y-%m-%d'))
+
+PLATFORM=$(uname -o)
+
+if [[ $PLATFORM == "Darwin" ]]; then
+    SINCE=$(date -v-1d +'%Y-%m-%d')
+else
+    SINCE=$(date -d '-1 day' +'%Y-%m-%d')
+fi
+
+COMMITS=$(git rev-list --count HEAD --since $SINCE)
 
 echo $DATE.$COMMITS
