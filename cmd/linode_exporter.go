da91053cebd1d927ed896a4540b39b846311751c
Joe Banks <joe@jb3.dev>
2025-10-23 03:24:07 +0100

    Initial pass at root command entrypoint

---
+++ cmd/linode_exporter.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"flag"
+	"github.com/jb3/linode_exporter/collector"
+	"github.com/linode/linodego"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"golang.org/x/oauth2"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+var (
+	listenAddress = flag.String("web.listen-address", ":9388", "Address to listen on for web interface and telemetry.")
+	metricsPath   = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
+	timeout       = flag.Duration("timeout", 10*time.Second, "Timeout for collecting metrics.")
+	apiToken      = flag.String("linode.token", "", "Linode API token (can also use LINODE_TOKEN env var).")
+)
+
+func main() {
+	flag.Parse()
+
+	token := *apiToken
+	if token == "" {
+		token = os.Getenv("LINODE_TOKEN")
+	}
+	if token == "" {
+		log.Fatal("Linode API token must be provided via -linode.token flag or LINODE_TOKEN environment variable")
+	}
+
+	tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	oauth2Client := oauth2.NewClient(nil, tokenSource)
+	linodeClient := linodego.NewClient(oauth2Client)
+
+	registry := prometheus.NewRegistry()
+
+	registry.MustRegister(collectors.NewGoCollector())
+	registry.MustRegister(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
+
+	linodeCollector := collector.NewLinodeCollector(&linodeClient, *timeout)
+	registry.MustRegister(linodeCollector)
+
+	http.Handle(*metricsPath, promhttp.HandlerFor(registry, promhttp.HandlerOpts{
+		ErrorLog:      log.New(os.Stderr, "", log.LstdFlags),
+		ErrorHandling: promhttp.ContinueOnError,
+		Registry:      registry,
+	}))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte(`<html>
+<head><title>Linode Exporter</title></head>
+<body>
+<h1>Linode Exporter</h1>
+<p><a href='` + *metricsPath + `'>Metrics</a></p>
+</body>
+</html>`))
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	log.Printf("Starting Linode exporter on %s", *listenAddress)
+	log.Printf("Metrics available at %s%s", *listenAddress, *metricsPath)
+
+	if err := http.ListenAndServe(*listenAddress, nil); err != nil {
+		log.Fatal(err)
+	}
+}
